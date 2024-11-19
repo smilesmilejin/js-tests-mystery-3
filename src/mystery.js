79b68af913e8206eb281cbc3a3c374913fa81d40
Ashley Yang <ashley.minghui@gmail.com>
2024-11-19 11:40:26 -0700

    Add solution

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,13 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  const len1 = string1.length;
+  // could check that the lengths are compatible
+
+  let diffs = 0;
+  for (let i = 0; i < len1; ++i) {
+    if (string1[i] !== string2[i]) {
+      ++diffs;
+    }
+  }
+
+  return diffs;
+};
