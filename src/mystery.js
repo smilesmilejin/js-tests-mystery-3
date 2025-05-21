e5092e43852025f0fd81dcf0bedf9bd7379a87af
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 14:21:32 -0400

    2025/05/21 Activity

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,13 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  if (!string1 && !string2) {
+    return 0;
+  };
+
+  let diff = 0;
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      diff += 1;
+    }
+  };
+  return diff;
+};
